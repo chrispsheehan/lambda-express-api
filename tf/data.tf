b6b53f875ceabd8557315241c7ff4496ce9029c0
chrispsheehan <chrispsheehan@gmail.com>
2024-01-09 20:31:53 +0000

    vars

--- tf/data.tf
+++ tf/data.tf
@@ -1,5 +1,5 @@
 data "archive_file" "source" {
   type        = "zip"
   source_dir  = "../src"
-  output_path = "../lambda-functions/loadbalancer-to-es.zip"
+  output_path = "../${var.lambda-folder}/${var.lambda-zip-path}"
 }
