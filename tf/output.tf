b11d3c5034ed8d3b2d48b071cf133d27af589993
chrispsheehan <chrispsheehan@gmail.com>
2024-01-10 15:58:42 +0000

    gateway url in output

--- tf/output.tf
+++ tf/output.tf
@@ -1,5 +1,5 @@
 output "api_gateway_url" {
-  value = "curl -X GET ${aws_api_gateway_deployment.this.invoke_url}${aws_api_gateway_stage.this.stage_name}/hello"
+  value = "${aws_api_gateway_deployment.this.invoke_url}${aws_api_gateway_stage.this.stage_name}"
 }
 
 output "function_name" {
