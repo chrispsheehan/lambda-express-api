96cefbacd92e87ee1fc3d199018aa10119f5eb26
chrispsheehan <chrispsheehan@gmail.com>
2024-01-10 09:51:30 +0000

    fixes

--- tf/main.tf
+++ tf/main.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "lambda-bucket" {
   bucket        = "${var.function-name}-bucket"
-  force_destroy = tr
 }
 
 resource "aws_s3_object" "lambda-zip" {
@@ -18,8 +17,6 @@ resource "aws_lambda_function" "test_lambda" {
   function_name = var.function-name
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.mjs"
-
-  source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "nodejs18.x"
 
