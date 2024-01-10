f769277190e409fc8d7bce68f82436750b67f9db
chrispsheehan <chrispsheehan@gmail.com>
2024-01-10 15:06:49 +0000

    fmt fix

--- tf/locals.tf
+++ tf/locals.tf
@@ -2,6 +2,6 @@ locals {
   lambda-runtime  = "nodejs18.x"
   lambda-name     = "${var.function-stage}-${var.function-name}"
   lambda-bucket   = "${local.lambda-name}-bucket"
-  lambda-folder   = "${local.lambda-name}"
+  lambda-folder   = local.lambda-name
   lambda-zip-path = "${local.lambda-name}.zip"
 }
