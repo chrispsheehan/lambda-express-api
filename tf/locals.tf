e39320650300e9f99e3a16897605f9f3772d4b5a
chrispsheehan <chrispsheehan@gmail.com>
2024-01-11 15:08:22 +0000

    fmt fix

--- tf/locals.tf
+++ tf/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  lambda-runtime  = "nodejs18.x"
-  lambda-name     = "${var.function-stage}-${var.function-name}"
-  lambda-bucket   = "${local.lambda-name}-bucket"
+  lambda-runtime = "nodejs18.x"
+  lambda-name    = "${var.function-stage}-${var.function-name}"
+  lambda-bucket  = "${local.lambda-name}-bucket"
 }
