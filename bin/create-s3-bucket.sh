b7ab31acb31145889004134c0d7a201652269722
chrispsheehan <chrispsheehan@gmail.com>
2024-01-15 15:40:07 +0000

    fix

--- bin/create-s3-bucket.sh
+++ bin/create-s3-bucket.sh
@@ -18,8 +18,8 @@ aws sts get-caller-identity
 bucket_exists=$(aws s3api list-buckets --query "Buckets[].Name" | jq -r --arg bucket_name $S3_BUCKET_NAME '. | index($bucket_name) != null')
 
 if [[ "$bucket_exists" == "true" ]]; then
-    echo "s3 bucket '$S3_STATE_NAME' found."
+    echo "s3 bucket '$S3_BUCKET_NAME' found."
 else
-    echo "s3 bucket '$S3_STATE_NAME' not found. Creating.."
-    aws s3api create-bucket --bucket $S3_STATE_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
+    echo "s3 bucket '$S3_BUCKET_NAME' not found. Creating.."
+    aws s3api create-bucket --bucket $S3_BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
 fi
