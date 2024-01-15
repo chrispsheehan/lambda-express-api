25feebabb7790e72cbcabda4c3623707d106e988
chrispsheehan <chrispsheehan@gmail.com>
2024-01-15 14:56:53 +0000

    create versions buckets

---
+++ bin/create-s3-bucket.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$S3_BUCKET_NAME" ]
+then
+    echo "S3_BUCKET_NAME not defined!"
+    exit 1
+fi
+
+echo creating s3 bucket $S3_BUCKET_NAME
+
+# check required cli tools
+aws --version
+jq --version
+
+#  output current caller ID
+aws sts get-caller-identity
+
+bucket_exists=$(aws s3api list-buckets --query "Buckets[].Name" | jq -r --arg bucket_name $S3_BUCKET_NAME '. | index($bucket_name) != null')
+
+if [[ "$bucket_exists" == "true" ]]; then
+    echo "s3 bucket '$S3_STATE_NAME' found."
+else
+    echo "s3 bucket '$S3_STATE_NAME' not found. Creating.."
+    aws s3api create-bucket --bucket $S3_STATE_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
+fi
