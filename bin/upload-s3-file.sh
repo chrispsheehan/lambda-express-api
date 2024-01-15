3ced6ca93a96a1639d29c6fd3206b4f5753dec47
chrispsheehan <chrispsheehan@gmail.com>
2024-01-15 15:43:29 +0000

    fix

--- bin/upload-s3-file.sh
+++ bin/upload-s3-file.sh
@@ -17,7 +17,7 @@ echo uploading $S3_FILE_PATH to $S3_BUCKET_NAME s3 bucket
 file_basename=$(basename "$S3_FILE_PATH")
 
 # Use the AWS CLI to upload the file to S3
-aws s3 cp "$S3_FILE_PATH" "s3://$S3_BUCKET_NAME/$file_basenam"
+aws s3 cp "$S3_FILE_PATH" "s3://$S3_BUCKET_NAME/$file_basename"
 
 # Check if the upload was successful
 if [ $? -eq 0 ]; then
