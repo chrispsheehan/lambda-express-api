e10920bc147020720d31f5d87cf06350c58f4382
chrispsheehan <chrispsheehan@gmail.com>
2024-01-17 11:05:56 +0000

    poc copy change

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const awsServerlessExpress = require('aws-serverless-express');
 const app = express();
 
 app.get('/hello', (req, res) => {
-  res.status(200).json({msg: "Hello, this is your AWS Lambda function, testy badgers!"});
+  res.status(200).json({msg: "Hello, this is your AWS Lambda function, [mushroom] testy badgers!"});
 });
 
 const server = awsServerlessExpress.createServer(app);
