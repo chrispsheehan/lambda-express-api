9d027c35d2d38e3210666ffa38cde35627da3f0e
chrispsheehan <chrispsheehan@gmail.com>
2024-02-23 15:19:48 +0000

    add logging

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ app.get('/hello', (req, res) => {
   res.status(200).json({msg: "Hello, this is your AWS Lambda function, [mushroom] testy badgers!"});
 });
 
+app.use((req, res, next) => { 
+  console.log(`Params: ${JSON.stringify(req.params)}`); // log out requests
+  console.log(`Headers: ${JSON.stringify(req.headers)}`);
+  console.log(`${req.method}: ${JSON.stringify(req.url)}`);
+  res.status(200).json({msg: "Hello, this is your AWS Lambda function, [mushroom] testy badgers!"});
+})
+
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => {
