50fe525ac9f38cd812564f5b4c43745fea5908e9
chrispsheehan <chrispsheehan@gmail.com>
2024-02-26 11:17:20 +0000

    principals

--- tf/data.tf
+++ tf/data.tf
@@ -13,6 +13,11 @@ data "aws_iam_policy_document" "assume_role" {
 
 data "aws_iam_policy_document" "whitelist_ips" {
   statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
     effect    = "Allow"
     actions   = ["execute-api:Invoke"]
     resources = [aws_api_gateway_rest_api.this.execution_arn]
